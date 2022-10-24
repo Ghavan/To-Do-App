d5825b9e4f7c90856b7e549ac440c6390e17d75e
Jayshah-JS1312 <jay.s@ahduni.edu.in>
2022-10-24 17:19:14 -0400

    todo App features added

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,9 @@
+const ACTIONS = {
+  LOGIN: "LOGIN",
+  GET_TOKEN: "GET_TOKEN",
+  GET_USER: "GET_USER",
+  GET_ALL_USERS: "GET_ALL_USERS",
+  ADD_NEW_TODO:"ADD_NEW_TODO"
+};
+
+export default ACTIONS;
---
+++ client/src/redux/store.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {createStore} from 'redux'
+import rootReducer from './reducers/'
+import {Provider} from 'react-redux'
+//import thunk from 'redux-thunk';
+
+//const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+function DataProvider({children}) {
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    )
+}
+
+export default DataProvider
+
+/*
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+*/
